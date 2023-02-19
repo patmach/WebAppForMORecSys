4e4a3ef718cc823ebb33779fe14840c0fa9eaedb
Your Name <you@example.com>
2023-02-20 00:26:07 +0100

    Přidání prvních prvků na domovskou stránku, zpracovávání datasetu

--- WebAppForMORecSys/Controllers/HomeController.cs
+++ WebAppForMORecSys/Controllers/HomeController.cs
@@ -1,22 +1,121 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
 using System.Diagnostics;
+using System.Globalization;
+using WebAppForMORecSys.Data;
 using WebAppForMORecSys.Models;
 
 namespace WebAppForMORecSys.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        private class MovieMap : ClassMap<Movie>
         {
-            _logger = logger;
+            public MovieMap()
+            {
+                Map(p => p.Id).Convert(args => int.Parse(args.Row.GetField("movieId")));
+                Map(p => p.Name).Index(1);
+                Map(p => p.JSONParams).Index(2);
+            }
         }
 
-        public IActionResult Index()
+        private class Link
         {
-            return View();
+           
+            public string Id { get; set; }
+            public string IMBDID { get; set; }
+            public string TMBDID { get; set; }
+            public Link()
+            {
+
+            }
+
+        }
+        private class LinkMap : ClassMap<Link>
+        {
+            public LinkMap()
+            {
+                Map(p => p.Id).Index(0);
+                Map(p => p.IMBDID).Index(1);
+                Map(p => p.TMBDID).Index(2);
+            }
+        }
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+            var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+            };
+            List<Movie> movies = new List<Movie>();
+            using (var reader = new StreamReader("Resources/Movielens/movies.csv"))
+            using (var csv = new CsvReader(reader, configuration))
+            {
+                csv.Context.RegisterClassMap<MovieMap>();
+                movies = csv.GetRecords<Movie>().ToList();
+            }
+            movies.ForEach(x => { x.JSONParams = "Genres: [" + x.JSONParams + ']'; });
+
+            List<Link> links = new List<Link>();
+            using (var reader = new StreamReader("Resources/Movielens/links.csv"))
+            using (var csv = new CsvReader(reader, configuration))
+            {
+                csv.Context.RegisterClassMap<LinkMap>();
+                links = csv.GetRecords<Link>().ToList();
+            }
+            
+
+        }
+
+        public class MainViewModel
+        {
+            public List<Metric> Metrics { get; set; }
+            public List<Item> Items { get; set; }
+
+            public MainViewModel()
+            {
+                this.Metrics = new List<Metric>();
+                this.Items = new List<Item>();
+            }
+        }
+        public async Task<IActionResult> Index(string search)
+        {
+            if (_context.Items == null)
+            {
+                return Problem("Entity set 'Context.Items'  is null.");
+            }
+            var viewModel = new MainViewModel();
+            var metrics = from metric in _context.Metrics
+                          select metric;
+            viewModel.Metrics = await metrics.ToListAsync();
+            viewModel.Metrics = new List<Metric> { new Metric { Name = "Relevance" },
+                new Metric { Name = "Novelty" }, new Metric { Name = "Diversity" } };//DELETE Later
+            var items = from item in _context.Items
+                         select item;
+            List<Item> items1;
+            List<Item> items2;
+            List<Item> items3;
+
+            if (!String.IsNullOrEmpty(search))
+            {
+                items1 = items.Where(s => s.Name!.Contains(search)).ToList();
+                items2 = items.Where(s => s.JSONParams!.Contains(search)).ToList();
+                items3 = items.Where(s => s.Description!.Contains(search)).ToList();
+                items1.AddRange(items2);
+                items1.AddRange(items3);
+                viewModel.Items = items1.Distinct().ToList();
+                return View(viewModel);
+            }
+            viewModel.Items =  await items.ToListAsync();
+            return View(viewModel);
         }
 
         [Authorize]
--- WebAppForMORecSys/Program.cs
+++ WebAppForMORecSys/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using WebAppForMORecSys.Areas.Identity.Data;
 using WebAppForMORecSys.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<Account>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 builder.Services.Configure<IdentityOptions>(options =>
