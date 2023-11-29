9a652134ad6eac061d4c4f1b031bb622be132ef7
Your Name <you@example.com>
2023-11-29 01:07:02 +0100

    Migration

---
+++ WebAppForMORecSys/Migrations/20231129000637_UserActModification.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAppForMORecSys.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserActModification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Done",
+                table: "UserActs");
+
+            migrationBuilder.DropColumn(
+                name: "NumberOfSuggestion",
+                table: "UserActs");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Done",
+                table: "UserActs",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfSuggestion",
+                table: "UserActs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
